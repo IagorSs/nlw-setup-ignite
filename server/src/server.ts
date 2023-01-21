2b8d983236697460e453897e38c0ad4e2acb8428
IagorSs <iagor.penaranda@uol.com.br>
2023-01-21 15:53:56 -0300

    feat(server): put basic configs

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,24 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-  return "Hello World"
+app.register(cors)
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany()
+
+  return habits
 })
 
+const PORT = process.env.PORT
+  ? parseInt(process.env.PORT)
+  : 5568
+
 app.listen({
-  port: 3256
+  port: PORT
 }).then(() => {
-  console.log('HTTP Server running!')
+  console.log(`HTTP Server running on http://localhost:${PORT} !`)
 })
