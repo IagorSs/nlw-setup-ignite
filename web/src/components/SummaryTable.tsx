72f172403ef2d3869ed0c37b375c4928f4961db8
IagorSs <iagor.penaranda@uol.com.br>
2023-01-22 09:44:24 -0300

    feat(summary-table): put key on map js

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -29,8 +29,8 @@ export default function SummaryTable() {
 
         {amountOfDaysToFill > 0
           && Array.from({ length: amountOfDaysToFill })
-            .map(() => (
-              <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
+            .map((_, i) => (
+              <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
             ))}
       </div>
     </div>
