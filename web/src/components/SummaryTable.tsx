947f2210deb38bfa41e3e74001141f717be5a3db
IagorSs <iagor.penaranda@uol.com.br>
2023-01-22 02:20:08 -0300

    feat(summary-table): implement minimum squares for screen

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -5,6 +5,9 @@ const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 
 const summaryDates = generateDatesFromYearBeginning()
 
+const minimumSummaryDatesSize = 18 * 7 // 18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
 export default function SummaryTable() {
   return (
     <div className="w-full flex">
@@ -23,6 +26,12 @@ export default function SummaryTable() {
         {summaryDates.map(summaryDate => (
           <HabitDay key={summaryDate.toString()} />
         ))}
+
+        {amountOfDaysToFill > 0
+          && Array.from({ length: amountOfDaysToFill })
+            .map(() => (
+              <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
+            ))}
       </div>
     </div>
   )
