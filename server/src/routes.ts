16c7551b222feaa67c4d092b78e174e293013481
IagorSs <iagor.penaranda@uol.com.br>
2023-01-22 01:35:16 -0300

    feat(server): get habits from day

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -28,4 +28,44 @@ export default async function appRoutes(app: FastifyInstance) {
       }
     })
   })
+
+  app.get('/day', async (req) => {
+    const getDayParams = z.object({
+      date: z.coerce.date()
+    })
+
+    const { date } = getDayParams.parse(req.query)
+    const parsedDate = dayjs(date).startOf('day')
+
+    const weekDay = parsedDate.get('day')
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay
+          }
+        }
+      }
+    })
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate()
+      },
+      include: {
+        dayHabits: true
+      }
+    })
+
+    const completedHabits = day?.dayHabits.map(dayHabit => dayHabit.habit_id)
+
+    return possibleHabits.map(possibleHabit => ({
+      ...possibleHabit,
+      completed: completedHabits?.includes(possibleHabit.id)
+    }))
+  })
 }
